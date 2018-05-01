b1b3e196b217b81c99f1d3fd286c142abf1f101d
dezwhatiwant <j.m.ledesma10@gmail.com>
2018-05-01 17:53:47 -0500

    changed employee, added fruits

--- employee.rb
+++ employee.rb
@@ -7,7 +7,45 @@
 # puts "#{employee_2[0]} #{employee_2[1]} makes #{employee_2[2]} dollars a year."
 
 
-employee_1 = {"first_name" => "Tim", "last_name" => "Taylor", "salary" => 70000, "active" => true}
-employee_2 = {:first_name => "Walt", :last_name => :Kronk, :salary => 80000, :active => true}
+# employee_1 = {"first_name" => "Tim", "last_name" => "Taylor", "salary" => 70000, "active" => true}
+# employee_2 = {:first_name => "Walt", :last_name => :Kronk, :salary => 80000, :active => true}
 
-puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} dollars a year."
+# puts "#{employee_2[:first_name]} #{employee_2[:last_name]} makes #{employee_2[:salary]} dollars a year."
+
+class Employee
+ attr_reader :first_name, :last_name, :salary, :active
+ attr_writer :first_name
+
+ def initialize(input_options) 
+  @first_name = input_options [:first_name]
+  @last_name = input_options [:last_name]
+  @salary = input_options [:salary]
+  @active = input_options [:active]
+ end
+
+
+  def print_info
+    puts "#{@first_name} #{@last_name} makes #{@salary} a year"
+  end
+
+  def give_annual_raise
+    @salary = 1.05 * @salary
+  end
+end
+
+employee_1 = Employee.new(
+                          first_name: "Tim", 
+                          last_name: "Taylor", 
+                          salary: 70000,
+                          active: true
+                          )
+
+employee_2 = Employee.new(
+                          first_name: "Walt",
+                          last_name: "Kronk",
+                          salary: 80000,
+                          active: true
+                          )
+
+employee_1.print_info
+employee_2.print_info
---
+++ fruits.rb
@@ -0,0 +1,17 @@
+fruits = [
+  {"name" => "apple", "color" => "red"},
+  {"name" => "banana", "color" => "yellow"},
+  {"name" => "grape", "color" => "purple"}
+]
+
+name_of_fruit = fruits.map { |part| part["name"]}
+
+color_of_fruit = fruits.map { |part| part["color"]}
+
+name_and_color = []
+
+name_and_color << [name_of_fruit[0]] + [color_of_fruit[0]]
+name_and_color << [name_of_fruit[1]] + [color_of_fruit[1]]
+name_and_color << [name_of_fruit[2]] + [color_of_fruit[2]]
+
+p name_and_color.to_h
